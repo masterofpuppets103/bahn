764dfd87a54feba004d2dc1fa166852a5c4d042a
masterofpuppets103 <info@schemelpictures.de>
2016-02-26 21:57:09 +0100

    Bug Fixes und zeigt nun nur noch die Zuege in die richtige Richtung an ;)

--- fahrplanauskunft.py
+++ fahrplanauskunft.py
@@ -1,5 +1,5 @@
 import requests
-
+from datetime import datetime
 bahnhof = raw_input("Bitte geben Sie den Startbahnhof ein: ")
 bahnhof2 = raw_input("Bitte geben Sie den Zielbahnhof ein: ")
 
@@ -10,7 +10,8 @@ try:
 	ort = data0['LocationList']['StopLocation'][0]['id']
 except KeyError:
 	ort = data0['LocationList']['StopLocation']['id']  
-	
+
+ortid = ort[2:]
 url3 = 'http://open-api.bahn.de/bin/rest.exe/location.name?authKey=DBhackFrankfurt0316&lang=de&input=' + bahnhof2 +'&format=json'
 
 data3 = requests.get(url3).json()
@@ -19,8 +20,10 @@ try:
 except KeyError:
 	ort2 = data3['LocationList']['StopLocation']['id'][2:]
 #print ort2
+time = datetime.now().strftime('%H:%M')
+date = datetime.now().strftime('%Y-%m-%d')
 
-url = 'http://open-api.bahn.de/bin/rest.exe/departureBoard?authKey=DBhackFrankfurt0316&lang=de&id=' + ort + '&date=2016-02-22&time=18:00&format=json'
+url = 'http://open-api.bahn.de/bin/rest.exe/departureBoard?authKey=DBhackFrankfurt0316&lang=de&id=' + ort + '&date=' + date + '&time=' +time +'&format=json'
 
 response = requests.get(url)
 
@@ -30,31 +33,27 @@ print "\n"+ "Verbindungen von " + bahnhof +" nach " + bahnhof2 +"\n"
 
 
 for l in data['DepartureBoard']['Departure']:
-	#print l
 	url2 = l['JourneyDetailRef']['ref']
 	response2 = requests.get(url2)
 	data2 = response2.json()
-	#print data2
-	#try:
 	zug = data2['JourneyDetail']['Names']['Name']['name']
+	before = False
 	for j in data2['JourneyDetail']['Stops']['Stop']:##damit findet er alle Zuege, die am Zielbahnhof vorbeifahren. Das ist nicht so ganz, was ich will :(
 		text4 = j['name']
 		try: 
-			text7 = j['track']
-		except KeyError: 
-			text7 = "unbekannt"
-		try: 
-			text5 = j['depTime']
-		except KeyError:
 			text5 = j['arrTime']
-		try: 
-			text6 = j['depDate']
 		except KeyError:
+			text5 = j['depTime']
+		try: 
 			text6 = j['arrDate']
+		except KeyError:
+			text6 = j['depDate']
 		try: 
 			text8 = j['id']
 		except KeyError:
 			text8 = ""
-		if (text8 == ort2):
-			print text4 + " um " + text5 +" am " + text6 + " auf Gleis " + text7 + " mit Zug " + zug
-
+		if (text8 == ortid):
+			before = True
+		if (text8 == ort2) and (before == True):
+			print "Abfahrt um " + l['time'] + " auf Gleis " + l['track'] +". Ankunft in " + text4 + " um " + text5 +" am " + text6 + " mit Zug " + zug
+	before = False
